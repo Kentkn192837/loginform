074f3e6ba18c0c2b6cbdee3eae499394c00953b1
髙野健太 <agriculture5012@gmail.com>
2020-10-03 23:19:36 +0900

    htmlを記述するPHPファイル群

---
+++ pages/registSucces.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<head>
+    <title>会員登録完了</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="form-wrapper">
+    <h1>会員登録完了</h1>
+    <p>会員登録が完了しました</p>
+    <a href="login.php">ログイン画面へ戻る</a>
+</div>
+</body>
+</html>
